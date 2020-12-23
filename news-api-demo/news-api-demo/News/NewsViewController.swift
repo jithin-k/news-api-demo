bf3190e98a857ba29b276d0dd4ceb29506e10c43
Jithin <jithinkunnil@qburst.com>
2020-12-23 19:23:28 +0530

    Add news view controller

---
+++ news-api-demo/news-api-demo/News/NewsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NewsViewController.swift
+//  news-api-demo
+//
+//  Created by Jithin on 23/12/20.
+//
+
+import UIKit
+
+class NewsViewController: UITableViewController {
+
+    var viewModel = NewsViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+}
---
+++ news-api-demo/news-api-demo/News/NewsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  NewsViewModel.swift
+//  news-api-demo
+//
+//  Created by Jithin on 23/12/20.
+//
+
+import Foundation
+
+class NewsViewModel {
+    
+}
